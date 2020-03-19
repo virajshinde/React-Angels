4c5cdc21d1babd34a90ea244504865e37eee7fa9
Viraj Abhay Shinde <virajabhayshinde@gmail.com>
2020-03-19 17:24:33 +0530

    Updates

---
+++ precache-manifest.abbc2c58d3814052316e53a7bfed1385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5be3ae4adab0ed2726130e11bb33ce4",
+    "url": "/React-Angels/index.html"
+  },
+  {
+    "revision": "40dc4e2211f590be2b95",
+    "url": "/React-Angels/static/css/main.0d5eb7ea.chunk.css"
+  },
+  {
+    "revision": "83c09f27fc2021014201",
+    "url": "/React-Angels/static/js/2.424742b0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Angels/static/js/2.424742b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40dc4e2211f590be2b95",
+    "url": "/React-Angels/static/js/main.91bc98ee.chunk.js"
+  },
+  {
+    "revision": "7ee5a21b041f17ef03e2",
+    "url": "/React-Angels/static/js/runtime-main.ed13b982.js"
+  }
+]);
